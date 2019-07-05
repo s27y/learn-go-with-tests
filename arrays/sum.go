4fc15b9aff0bcb89aba03b71243a4df73b5813b6
Yang Sun <yang.sun@jet.com>
2019-07-05 13:56:32 +0100

    complete arrays

---
+++ arrays/sum.go
@@ -0,0 +1,30 @@
+package arrays
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSumTails ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSumTails {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+}
