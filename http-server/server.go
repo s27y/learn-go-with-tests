57647d40ec0e68b1c4f950648355ed36c28cc19f
Yang Sun <s27y@hotmail.com>
2019-07-12 11:22:34 +0100

    Complete http server

--- http-server/server.go
+++ http-server/server.go
@@ -5,23 +5,38 @@ import (
 	"net/http"
 )
 
-// type Handler interface {
-// 	ServeHTTP(http.ResponseWriter, *http.Request)
-// }
+type PlayerServer struct {
+	store PlayerStore
+}
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
-	fmt.Fprint(w, GetPlayerScore(player))
-	return
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
 
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
 	}
-	if name == "Floyd" {
-		return "10"
-	}
-	return ""
+
+	fmt.Fprint(w, score)
 }
